e3af593563d7881f4960dceaf0fb4bfe5d9ba82d
Aniketsharma1203 <122670703+Aniketsharma1203@users.noreply.github.com>
2024-05-20 02:23:03 +0530

    All done.

--- script.js
+++ script.js
@@ -5,12 +5,29 @@ function addRow() {
     const pan = document.getElementById('pan').value.trim();
     const dob = document.getElementById('dob').value;
     const tel = document.getElementById('tel').value.trim();
+    const pinCode = document.getElementById('pin-code').value.trim();
 
     // Check if any of the required fields are empty
-    if (name === '' || pan === '' || dob === '' || tel === '') {
+    if (name === '' || pan === '' || dob === '' || tel === '' || pinCode === '') {
         alert('Please fill in all required fields.');
         return; // Stop execution if any required field is empty
     }
+    // Validate PAN length
+    if (pan.length > 10 || pan.length > 10) {
+        alert('PAN should be of 10 characters.');
+        return;
+    }
+
+    // Validate Pin Code length
+    if (pinCode.length > 6 || pinCode.length < 6) {
+        alert('Pin Code should be of 6 digits.');
+        return;
+    }
+    if (tel.length > 10 || tel.length < 10) {
+        alert('Tel. should be of 10 digits.');
+        return;
+    }
+
 
     const tableBody = document.getElementById('table-body');
 
@@ -30,7 +47,8 @@ function addRow() {
                 <span class="tooltiptext">
                     <strong>PAN:</strong> ${pan}<br>
                     <strong>DOB:</strong> ${dob}<br>
-                    <strong>Tel:</strong> ${tel}
+                    <strong>Tel:</strong> ${tel}<br>
+                    <strong>Pin:</strong> ${pinCode}
                 </span>
             </div>
         </td>
@@ -52,6 +70,7 @@ function clearFields() {
     document.getElementById('pan').value = '';
     document.getElementById('dob').value = '';
     document.getElementById('tel').value = '';
+    document.getElementById('pin-code').value = '';
 }
 
 function editRow(button) {
@@ -83,21 +102,12 @@ function deleteRow(button) {
         tableBody.appendChild(noRecordsRow);
     }
 
-    rowCount--;
     if (tableBody.rows.length === 0) {
         // Hide footer text if no records
         const footer = document.querySelector('footer');
         footer.style.display = 'none';
     }
 
-}
+    rowCount--;
 
-function resetTable() {
-    const tableBody = document.getElementById('table-body');
-    tableBody.innerHTML = `
-        <tr id="no-records">
-            <td colspan="3">No records available</td>
-        </tr>
-    `;
-    rowCount = 1;
 }
